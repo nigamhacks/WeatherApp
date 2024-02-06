a13e66cc6487fba7c64ff159abab88c8ce6ede35
sachin <sachinnigam9@live.com>
2024-02-06 15:52:16 +0530

    second commit

--- App.js
+++ App.js
@@ -1,67 +1,20 @@
-import React from "react";
-import {View, Text, SafeAreaView, StyleSheet } from 'react-native';
-//vector libray using for icons and it is installed by default 
-//when creating expo project or App 
-import {Feather} from '@expo/vector-icons';
+import React from 'react'
+import {View, StyleSheet} from 'react-native'
+import CurrentWeather from './src/components/CurrentWeather';
 
 const App=()=>{
-return (
-  <SafeAreaView style={styles.wrapper}> 
+return(
   <View style={styles.container}>
-    <Feather name='sun' size={100}></Feather>
-    <Text>Current Weather</Text>
-    <Text style={styles.temp}>6</Text>
-    <Text style={styles.feels}>Feels like 5</Text>
-    <Text style={styles.lowHigh}>High: 8 low: 6</Text>
+    <CurrentWeather/>
   </View>
+)
+};
 
-  <View style={styles.bodyWrapper}>
-    <Text style={styles.description}>
-      It's Sunny !
-    </Text>
-    <Text style={styles.message}>
-      It's Perfect T-shirt Weather 
-    </Text>
-  </View>
-  </SafeAreaView>
-);
-}
 const styles=StyleSheet.create({
-  wrapper:{
-    backgroundColor:'#e6e6fa',// lavender 
-    flex:1,
-    
-  },
   container:{
-    
-    flex:1,
-    alignItems:'center'
-  },
-  
-  temp:{
-    color:'black',
-    fontSize:48,
-    alignItems:'center'
-  },
-  feels:{
-    fontSize:30, 
-    color:'black',
-  }, 
-  lowHigh:{
-    fontSize:10, 
-
-  },
-  bodyWrapper:{
-    paddingLeft:25, 
-    marginBottom:10,
-    justifyContent:'flex-end', 
-    alignItems:'flex-start'
-  },
-  description:{
-    fontSize:48
-  },
-  message:{
-    fontSize:30
+  flex:1
   }
+
 })
+
 export default App;
